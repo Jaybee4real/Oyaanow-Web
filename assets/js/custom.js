a9eff91c99d9accc7cc86455de83529fbe5edc87
Jaybee4real <okorojijaybee1@gmail.com>
2020-04-10 18:56:14 +0100

    _made nav scroll effect and mobile menu

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+// let navbarObserver = new IntersectionObserver ()
+let scroll = window.pageYOffset;
+
+document.addEventListener("scroll", function (e) {
+let navbar = document.getElementById("navbar")
+	var offset = window.pageYOffset;
+	scroll = offset;
+		if (scroll > 1) {
+            navbar.classList.add("scroll")
+            	
+		}
+	else {
+		navbar.classList.remove("scroll")
+	}
+})
