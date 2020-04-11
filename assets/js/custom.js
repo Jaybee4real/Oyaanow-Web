cedf9e310e5aab205ec301580e1b6c3f66630f55
Jaybee4real <okorojijaybee1@gmail.com>
2020-04-11 20:42:03 +0100

    _section-3 modification

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,30 @@
+
+            window.setTimeout(function clear() {
+                document.querySelector(".loader").classList.add("loaded")
+            }, 2000
+            )
+            let navButton = document.querySelector(".hamburger")
+            navButton.addEventListener("click", () => {
+                document.querySelector(".mobile-menu").classList.toggle("open");
+                navButton.classList.toggle("fa-bars")
+                navButton.classList.toggle("fa-times")
+            })
+
+            const slideUp = document.querySelector(".slide-up")
+
+            let options = {
+                rootMargin: '700px',
+                threshold: 1.0
+            }
+
+            let observer = new IntersectionObserver((entries) => {
+                console.log(entries)
+            }, options);
+
+
+            observer.observe(slideUp)
+
+
 // let navbarObserver = new IntersectionObserver ()
 let scroll = window.pageYOffset;
 
@@ -15,3 +42,5 @@ let navbar = document.getElementById("navbar")
 })
 
 
+
+//////////////////////////////Intersection Observer////////////////
