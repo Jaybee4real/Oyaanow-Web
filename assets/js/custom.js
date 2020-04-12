84cd0575d744ca1211ead54639b5dfb29bf9ab4c
Jaybee4real <okorojijaybee1@gmail.com>
2020-04-12 17:43:07 +0100

    _added header animation and sample logo(to be removed)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,41 +1,59 @@
 
-            window.setTimeout(function clear() {
-                document.querySelector(".loader").classList.add("loaded")
-            }, 2000
-            )
-            let navButton = document.querySelector(".hamburger")
-            navButton.addEventListener("click", () => {
-                document.querySelector(".mobile-menu").classList.toggle("open");
-                navButton.classList.toggle("fa-bars")
-                navButton.classList.toggle("fa-times")
-            })
-
-            const slideUp = document.querySelector(".slide-up")
-
-            let options = {
-                rootMargin: '700px',
-                threshold: 1.0
-            }
-
-            let observer = new IntersectionObserver((entries) => {
-                console.log(entries)
-            }, options);
 
 
-            observer.observe(slideUp)
+var images = ["Hiace.jpg", "Bus_fleet.jpg", "bus_side.jpg"];
+$(function () {
+	var i = 0;
+	$(".header").css("background-image", "url(./assets/img/" + images[i] + ")");
+
+	setInterval(function () {
+		i++;
+		if (i == images.length) {
+			i = 0;
+		}
+		$(".header").css("background-image", "url(./assets/img/" + images[i] + ")");
+	}, 5000);
+});
+
+
+
+window.setTimeout(function clear() {
+	document.querySelector(".loader").classList.add("loaded")
+}, 2000
+)
+let navButton = document.querySelector(".hamburger")
+navButton.addEventListener("click", () => {
+	document.querySelector(".mobile-menu").classList.toggle("open");
+	navButton.classList.toggle("fa-bars")
+	navButton.classList.toggle("fa-times")
+})
+
+const slideUp = document.querySelector(".slide-up")
+
+let options = {
+	rootMargin: '700px',
+	threshold: 1.0
+}
+
+let observer = new IntersectionObserver((entries) => {
+	console.log(entries)
+}, options);
+
+
+observer.observe(slideUp)
 
 
 // let navbarObserver = new IntersectionObserver ()
 let scroll = window.pageYOffset;
 
 document.addEventListener("scroll", function (e) {
-let navbar = document.getElementById("navbar")
+	let navbar = document.getElementById("navbar")
 	var offset = window.pageYOffset;
 	scroll = offset;
-		if (scroll > 1) {
-            navbar.classList.add("scroll")
-            	
-		}
+	if (scroll > 1) {
+		navbar.classList.add("scroll")
+
+	}
 	else {
 		navbar.classList.remove("scroll")
 	}
